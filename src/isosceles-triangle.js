6f7a54116ef8f56c422a9b1fad7174022d09b626
nammon72 <namnh.vst@gmail.com>
2022-07-03 15:09:20 +0700

    feat: draw inverted triangle

--- src/isosceles-triangle.js
+++ src/isosceles-triangle.js
@@ -1,16 +1,32 @@
-function drawIsoscelesTriangle(h) {
+// function drawIsoscelesTriangle(h) {
+//     let shape = "";
+//     for(let i = 1; i <= h; i++) {
+//         // print space
+//         for(let j = 1; j<= h-i; j++) {
+//             shape += " ";
+//         }
+//         // print star "*"
+//         for(let k = 1; k <= 2*i-1; k++) {
+//             shape += "*";
+//         }
+//         shape += "\n";
+//     }
+//     return shape;
+// }
+// console.log(drawIsoscelesTriangle(10));
+
+function drawTriangle(n) {
     let shape = "";
-    for(let i = 1; i <= h; i++) {
-        // print space
-        for(let j = 1; j<= h-i; j++) {
+    for(let i = n; i > 0; i--) {
+        for(let j = n; j > i; j--) {
             shape += " ";
         }
-        // print star "*"
-        for(let k = 1; k <= 2*i-1; k++) {
+        for(let k = 1; k <= 2*i - 1 ; k++) {
             shape += "*";
         }
         shape += "\n";
     }
     return shape;
 }
-console.log(drawIsoscelesTriangle(10));
+
+console.log(drawTriangle(10)); 
