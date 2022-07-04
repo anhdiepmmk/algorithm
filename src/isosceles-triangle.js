040c1103d3195fd9bea83ed7eeaebc1f2881926e
nammon72 <namnh.vst@gmail.com>
2022-07-04 14:30:34 +0700

    feat: draw pascal triangle

--- src/isosceles-triangle.js
+++ src/isosceles-triangle.js
@@ -1,16 +1,21 @@
-function drawIsoscelesTriangle(h) {
+
+function pascal(k, n) {
+    if(k == 0 || k == n) {
+        return 1;
+    } else {
+        return pascal(k, n - 1) + pascal(k - 1, n - 1);
+    }
+};
+
+function drawAPascalTriangle(n) {
     let shape = "";
-    for(let i = 1; i <= h; i++) {
-        // print space
-        for(let j = 1; j<= h-i; j++) {
-            shape += " ";
-        }
-        // print star "*"
-        for(let k = 1; k <= 2*i-1; k++) {
-            shape += "*";
+    for(let i = 0; i <= n; i++) {
+        for(let k = 0; k <= i; k++) {
+            shape += pascal(k, i);
         }
         shape += "\n";
     }
     return shape;
-}
-console.log(drawIsoscelesTriangle(10));
+};
+
+console.log(drawAPascalTriangle(10));
