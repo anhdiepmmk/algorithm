137cdd922a13cbc9a0483dba4263258d9341ab7d
Diep <n08ni.dieppn@gmail.com>
2022-07-04 22:10:42 +0700

    style: setup prettier
    - add prettier configuration file
    
    - add .gitignore file
    
    - format all existing code

--- src/isosceles-triangle.js
+++ src/isosceles-triangle.js
@@ -1,16 +1,16 @@
 function drawIsoscelesTriangle(h) {
-    let shape = "";
-    for(let i = 1; i <= h; i++) {
-        // print space
-        for(let j = 1; j<= h-i; j++) {
-            shape += " ";
-        }
-        // print star "*"
-        for(let k = 1; k <= 2*i-1; k++) {
-            shape += "*";
-        }
-        shape += "\n";
-    }
-    return shape;
+	let shape = "";
+	for (let i = 1; i <= h; i++) {
+		// print space
+		for (let j = 1; j <= h - i; j++) {
+			shape += " ";
+		}
+		// print star "*"
+		for (let k = 1; k <= 2 * i - 1; k++) {
+			shape += "*";
+		}
+		shape += "\n";
+	}
+	return shape;
 }
 console.log(drawIsoscelesTriangle(10));
--- src/triangle.js
+++ src/triangle.js
@@ -1,12 +1,12 @@
 function draw(h) {
-  let shape = "";
-  for (let i = 0; i < h; i++) {
-    for (let j = 0; j < i; j++) {
-      shape += "*";
-    }
-    shape += "\n";
-  }
-  return shape;
+	let shape = "";
+	for (let i = 0; i < h; i++) {
+		for (let j = 0; j < i; j++) {
+			shape += "*";
+		}
+		shape += "\n";
+	}
+	return shape;
 }
 
 console.log(draw(10));
