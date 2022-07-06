326417a19289df421fc0e68b6cdd046834810ee4
nammon72 <namnh.vst@gmail.com>
2022-07-06 09:54:18 +0700

    feat: draw the Pascal triangle

---
+++ src/pascal-triangle.js
@@ -0,0 +1,20 @@
+function pascal(k, n) {
+    if(k == 0 || k == n) {
+        return 1;
+    } else {
+        return pascal(k, n - 1) + pascal(k - 1, n - 1);
+    }
+};
+
+function drawPascalTriangle(n) {
+    let shape = "";
+    for(let i = 0; i <= n; i++) {
+        for(let k = 0; k <= i; k++) {
+            shape += pascal(k, i);
+        }
+        shape += "\n";
+    }
+    return shape;
+};
+
+console.log(drawPascalTriangle(10));
