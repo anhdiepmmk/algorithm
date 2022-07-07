b43e49c8e7b9f1c7ee3ffe4996e3847a1c59c63e
nammon72 <namnh.vst@gmail.com>
2022-07-07 15:05:31 +0700

    fix: create unit test branch from feature/the-factorial-of-a-positive-integer

--- src/inverted-triangle.test.js
+++ src/inverted-triangle.test.js
@@ -1,23 +1,23 @@
-const triangle = require('./inverted-triangle')
+const triangle = require('./inverted-triangle');
 
 describe('inverted-triangle', () => {
 	describe('draw', ()=>{
-		test('should draw in inverted triangle when given height = 4', ()=> {
+		test('should draw in inverted triangle when given height = 5', ()=> {
 			const height = 5;
+			const expectedResult = `*****\n *** \n  *  `;
+			const result = triangle.drawInvertedTriangle(height);
+			expect(result).toBe(expectedResult);
+		});
+
+		test('should draw in inverted triangle when given height = 0', ()=> {
+			const height = 0;
 			const expectedResult = ``;
 			const result = triangle.drawInvertedTriangle(height);
 			expect(result).toBe(expectedResult);
 		});
 
-		test('should draw in inverted triangle when given height = 4', ()=> {
-			const height = 5;
-			const expectedResult = ``;
-			const result = triangle.drawInvertedTriangle(height);
-			expect(result).toBe(expectedResult);
-		});
-
-		test('should draw in inverted triangle when given height = 4', ()=> {
-			const height = 5;
+		test('should draw in inverted triangle when given height = -1', ()=> {
+			const height = -1;
 			const expectedResult = ``;
 			const result = triangle.drawInvertedTriangle(height);
 			expect(result).toBe(expectedResult);
