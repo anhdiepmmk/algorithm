43359178007c5b52728a632aa7d1720967178b50
nammon72 <namnh.vst@gmail.com>
2022-07-07 11:56:24 +0700

    test: test inverted triangle

---
+++ src/inverted-triangle.test.js
@@ -0,0 +1,26 @@
+const triangle = require('./inverted-triangle')
+
+describe('inverted-triangle', () => {
+	describe('draw', ()=>{
+		test('should draw in inverted triangle when given height = 4', ()=> {
+			const height = 5;
+			const expectedResult = ``;
+			const result = triangle.drawInvertedTriangle(height);
+			expect(result).toBe(expectedResult);
+		});
+
+		test('should draw in inverted triangle when given height = 4', ()=> {
+			const height = 5;
+			const expectedResult = ``;
+			const result = triangle.drawInvertedTriangle(height);
+			expect(result).toBe(expectedResult);
+		});
+
+		test('should draw in inverted triangle when given height = 4', ()=> {
+			const height = 5;
+			const expectedResult = ``;
+			const result = triangle.drawInvertedTriangle(height);
+			expect(result).toBe(expectedResult);
+		});
+	});
+});
---
+++ src/isosceles-triangle.test.js
@@ -0,0 +1,32 @@
+const isosceles = require('./isosceles-triangle')
+
+describe('isosceles', () => {
+	describe('draw', () => {
+		test('should draw triangle when given height = 4', () => {
+			const height = 4;
+			const expectedResult = `   *\n  ***\n *****\n*******`;
+
+			const result = isosceles.drawIsoscelesTriangle(height);
+
+			expect(result).toBe(expectedResult);
+		});
+
+		test('should draw triangle when given height = 0', () => {
+			const height = 0;
+			const expectedResult = ``;
+
+			const result = isosceles.drawIsoscelesTriangle(height);
+
+			expect(result).toBe(expectedResult);
+		});
+
+		test('should draw triangle when given height = -1', () => {
+			const height = -1;
+			const expectedResult = ``;
+
+			const result = isosceles.drawIsoscelesTriangle(height);
+
+			expect(result).toBe(expectedResult);
+		});
+	});
+});
