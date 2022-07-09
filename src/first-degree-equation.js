88e140feb7c7c29d1ea8e9d7e39d71b0d1b2278a
nammon72 <namnh.vst@gmail.com>
2022-07-09 12:02:38 +0700

    fix: the first-degree equation

--- src/first-degree-equation.js
+++ src/first-degree-equation.js
@@ -5,7 +5,9 @@ function solveFirstDegreeEquation(number1, number2) {
     return 0;
   } else if (number1 === 0 && number2 !== 0) {
     return 'The equation has no solutions';
-  } else return -number2 / number1;
+  } else {
+    return -number2 / number1;
+  }
 }
 
 module.exports = {
