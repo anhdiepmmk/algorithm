42de9a554146c08b1b6e5ad6ea1ae9d2584d241e
nammon72 <namnh.vst@gmail.com>
2022-07-09 11:03:37 +0700

    feat: the first-degree equation

--- src/first-degree-equation.js
+++ src/first-degree-equation.js
@@ -1,17 +1,12 @@
 function solveFirstDegreeEquation(number1, number2) {
-  let result = '';
-  let temp = 0;
+  let result = 0;
   if (number1 === 0 && number2 === 0) {
-    return result;
+    return 'The equation has infinitely many solutions';
   } else if (number1 !== 0 && number2 === 0) {
-    result += temp;
+    return 0;
   } else if (number1 === 0 && number2 !== 0) {
-    return result;
-  } else {
-    temp = -number2 / number1;
-    result += temp;
-  }
-  return result;
+    return 'The equation has no solutions';
+  } else return -number2 / number1;
 }
 
 module.exports = {
--- src/first-degree-equation.test.js
+++ src/first-degree-equation.test.js
@@ -4,7 +4,7 @@ describe('solve-the-first-degree-equation', () => {
   test('should return 0.5 when given a = 5, b = 2', () => {
     const number1 = 5;
     const number2 = 2;
-    const expectedResult = '-0.4';
+    const expectedResult = -0.4;
     const result = firstDegreeEquation.solveFirstDegreeEquation(
       number1,
       number2,
@@ -12,10 +12,10 @@ describe('solve-the-first-degree-equation', () => {
     expect(result).toBe(expectedResult);
   });
 
-  test('should return "" when given number1 = 0 && number2 = 0', () => {
+  test('should return "The equation has infinitely many solutions" when given number1 = 0 && number2 = 0', () => {
     const number1 = 0;
     const number2 = 0;
-    const expectedResult = '';
+    const expectedResult = 'The equation has infinitely many solutions';
     const result = firstDegreeEquation.solveFirstDegreeEquation(
       number1,
       number2,
@@ -26,7 +26,7 @@ describe('solve-the-first-degree-equation', () => {
   test('should return 0 when given number1 !=0 && number2 = 0', () => {
     const number1 = 1;
     const number2 = 0;
-    const expectedResult = '0';
+    const expectedResult = 0;
     const result = firstDegreeEquation.solveFirstDegreeEquation(
       number1,
       number2,
@@ -34,10 +34,10 @@ describe('solve-the-first-degree-equation', () => {
     expect(result).toBe(expectedResult);
   });
 
-  test('should return "" when given number1 = 0 && number2 != 0', () => {
+  test('should return the equation has no solution when given number1 = 0 && number2 != 0', () => {
     const number1 = 0;
     const number2 = 2;
-    const expectedResult = '';
+    const expectedResult = 'The equation has no solutions';
     const result = firstDegreeEquation.solveFirstDegreeEquation(
       number1,
       number2,
