5b5b714e3ad81e3d7c590944afea4a87ea7596fd
nammon72 <namnh.vst@gmail.com>
2022-07-12 00:20:38 +0700

    fix: rename function, variable for clear

--- src/reverse-number.js
+++ src/reverse-number.js
@@ -1,15 +1,15 @@
-function returnReverseNumber(n) {
+function reverseNumber(n) {
   let number = n;
   let temp;
-  let reverseNumber = 0;
+  let reversalNumber = 0;
   while (number > 0) {
     temp = number % 10;
-    reverseNumber = reverseNumber * 10 + temp;
+    reversalNumber = reversalNumber * 10 + temp;
     number = Math.floor(number / 10);
   }
-  return reverseNumber;
+  return reversalNumber;
 }
 
 module.exports = {
-  returnReverseNumber,
+  reverseNumber,
 };
