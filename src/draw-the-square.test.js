bdaaa66c3022ffee8dbd1c7b5da466abbebb42df
nammon72 <namnh.vst@gmail.com>
2022-07-12 15:24:37 +0700

    fix: draw-the-square #42

--- src/draw-the-square.test.js
+++ src/draw-the-square.test.js
@@ -1,70 +1,61 @@
 const square = require('./draw-the-square');
 
-describe('draw-the-square', () => {
-  test('should-draw-the-solid-square-when-n=5', () => {
+describe('draw the square', () => {
+  test('should draw the solid square when n=5', () => {
     const n = 5;
     const expectedResult = '*****\n*****\n*****\n*****\n*****';
     const result = square.drawSolidSquare(n);
     expect(result).toEqual(expectedResult);
   });
-
-  test('should-return-""-when-n=0', () => {
+  test('should return "" when n=0', () => {
     const n = 0;
     const expectedResult = '';
     const result = square.drawSolidSquare(n);
     expect(result).toEqual(expectedResult);
   });
-
-  test('should-return-""-when-n=0', () => {
+  test('should return "" when n=0', () => {
     const n = 0;
     const expectedResult = '';
     const result = square.drawEmptySquare(n);
     expect(result).toEqual(expectedResult);
   });
-
-  test('should-return-"*"-when-n=1', () => {
+  test('should return "*" when n=1', () => {
     const n = 1;
     const expectedResult = '*';
     const result = square.drawSolidSquare(n);
     expect(result).toEqual(expectedResult);
   });
-
-  test('should-return-"*"-when-n=1', () => {
+  test('should return "*" when n=1', () => {
     const n = 1;
     const expectedResult = '*';
     const result = square.drawEmptySquare(n);
     expect(result).toEqual(expectedResult);
   });
-
-  test('should-draw-the-solid-square-when-n=2', () => {
+  test('should draw the solid square when n=2', () => {
     const n = 2;
     const expectedResult = '**\n**';
     const result = square.drawSolidSquare(n);
     expect(result).toEqual(expectedResult);
   });
-
-  test('should-draw-the-solid-square-when-n=2', () => {
+  test('should draw the solid square when n=2', () => {
     const n = 2;
     const expectedResult = '**\n**';
     const result = square.drawEmptySquare(n);
     expect(result).toEqual(expectedResult);
   });
-
-  test('sshould-draw-the-empty-square-when-n=3', () => {
+  test('sshould draw the empty square when n=3', () => {
     const n = 3;
     const expectedResult = '***\n* *\n***';
     const result = square.drawEmptySquare(n);
     expect(result).toEqual(expectedResult);
   });
-
-  test('sshould-draw-the-empty-square-when-n=3', () => {
+  test('sshould draw the empty square when n=3', () => {
     const n = 3;
     const expectedResult = '***\n***\n***';
     const result = square.drawSolidSquare(n);
     expect(result).toEqual(expectedResult);
   });
-
-  test('should-draw-the-empty-square-when-n=5', () => {
+  test('should draw the empty square when n=5', () => {
     const n = 5;
     const expectedResult = '*****\n*   *\n*   *\n*   *\n*****';
     const result = square.drawEmptySquare(n);
