1cf35e1a4f81c661a8ef0aa65fb7448c542a9927
nammon72 <namnh.vst@gmail.com>
2022-07-13 13:50:51 +0700

    fix: reformat test case

--- src/check-primes.test.js
+++ src/check-primes.test.js
@@ -1,53 +1,20 @@
 const primeNumber = require('./check-primes');
 
 describe('check-primes', () => {
-  test('should return true when given 5', () => {
-    const n = 5;
-    const expectedResult = true;
-    const result = primeNumber.isPrimeNumber(n);
-
-    expect(result).toBe(expectedResult);
+  test.each([
+    { n: 3, expected: true },
+    { n: 5, expected: true },
+    { n: 59, expected: true },
+    { n: 113, expected: true },
+  ])('should return true when given $n', ({ n, expected }) => {
+    expect(primeNumber.isPrimeNumber(n)).toEqual(expected);
   });
-  test('should return true when given 59', () => {
-    const n = 59;
-    const expectedResult = true;
-    const result = primeNumber.isPrimeNumber(n);
-
-    expect(result).toBe(expectedResult);
-  });
-  test('should return true when given 113', () => {
-    const n = 113;
-    const expectedResult = true;
-    const result = primeNumber.isPrimeNumber(n);
-
-    expect(result).toBe(expectedResult);
-  });
-  test('should return false when given 0', () => {
-    const n = 0;
-    const expectedResult = false;
-    const result = primeNumber.isPrimeNumber(n);
-
-    expect(result).toBe(expectedResult);
-  });
-  test('should return false when given 1', () => {
-    const n = 1;
-    const expectedResult = false;
-    const result = primeNumber.isPrimeNumber(n);
-
-    expect(result).toBe(expectedResult);
-  });
-  test('should return false when given 100', () => {
-    const n = 100;
-    const expectedResult = false;
-    const result = primeNumber.isPrimeNumber(n);
-
-    expect(result).toBe(expectedResult);
-  });
-  test('should return false when given 123', () => {
-    const n = 123;
-    const expectedResult = false;
-    const result = primeNumber.isPrimeNumber(n);
-
-    expect(result).toBe(expectedResult);
+  test.each([
+    { n: 0, expected: false },
+    { n: 1, expected: false },
+    { n: 100, expected: false },
+    { n: 123, expected: false },
+  ])('should return false when given $n', ({ n, expected }) => {
+    expect(primeNumber.isPrimeNumber(n)).toEqual(expected);
   });
 });
