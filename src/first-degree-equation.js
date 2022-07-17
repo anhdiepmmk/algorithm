dd7f473f232e4c33aee8257dd7adcc2ab8782ac0
Pham Ngoc Diep <diep.pham@your.rentals>
2022-07-17 12:31:34 +0700

    feat: eslint integrated with prettier

--- src/first-degree-equation.js
+++ src/first-degree-equation.js
@@ -1,11 +1,16 @@
 function solveFirstDegreeEquation(number1, number2) {
   if (number1 === 0 && number2 === 0) {
     return 'The equation has infinitely many solutions';
-  } if (number1 !== 0 && number2 === 0) {
+  }
+
+  if (number1 !== 0 && number2 === 0) {
     return 0;
-  } if (number1 === 0 && number2 !== 0) {
+  }
+
+  if (number1 === 0 && number2 !== 0) {
     return 'The equation has no solutions';
   }
+
   return -number2 / number1;
 }
 
