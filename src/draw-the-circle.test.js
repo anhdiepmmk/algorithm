787540c824d1d0e6a6e4945a1ac379d9678023f3
nammon72 <namnh.vst@gmail.com>
2022-07-17 13:15:57 +0700

    fix: reformat test case

--- src/draw-the-circle.test.js
+++ src/draw-the-circle.test.js
@@ -3,39 +3,47 @@ const circle = require('./draw-the-circle');
 describe('Circle', () => {
   test('should draw the circle when given a radius = 5', () => {
     const radius = 5;
-    const expectedResult = '.....-.....\n'
-      + '..-*****-..\n'
-      + '.-*******-.\n'
-      + '.*********.\n'
-      + '.*********.\n'
-      + '-*********-\n'
-      + '.*********.\n.'
-      + '*********.\n'
-      + '.-*******-.\n'
-      + '..-*****-..\n'
-      + '.....-.....';
+    const expected = '.....-.....\n'
+                   + '..-*****-..\n'
+                   + '.-*******-.\n'
+                   + '.*********.\n'
+                   + '.*********.\n'
+                   + '-*********-\n'
+                   + '.*********.\n'
+                   + '.*********.\n'
+                   + '.-*******-.\n'
+                   + '..-*****-..\n'
+                   + '.....-.....';
     const result = circle.drawCircle(radius);
-    expect(result).toEqual(expectedResult);
+    expect(result).toEqual(expected);
   });
 
   test('should draw the circle when given a radius = 3', () => {
     const radius = 3;
-    const expectedResult = '...-...\n.*****.\n.*****.\n-*****-\n.*****.\n.*****.\n...-...';
+    const expected = '...-...\n'
+                   + '.*****.\n'
+                   + '.*****.\n'
+                   + '-***** -\n'
+                   + '.*****.\n'
+                   + '.*****.\n'
+                   + '...-...';
     const result = circle.drawCircle(radius);
-    expect(result).toEqual(expectedResult);
+    expect(result).toEqual(expected);
   });
 
   test('should draw the circle when given a radius = 1', () => {
     const radius = 1;
-    const expectedResult = '.-.\n-*-\n.-.';
+    const expected = '.-.\n'
+                   + '-* -\n'
+                   + '.-.';
     const result = circle.drawCircle(radius);
-    expect(result).toEqual(expectedResult);
+    expect(result).toEqual(expected);
   });
 
   test('should draw the circle when given a radius = 0', () => {
     const radius = 0;
-    const expectedResult = '-';
+    const expected = '-';
     const result = circle.drawCircle(radius);
-    expect(result).toEqual(expectedResult);
+    expect(result).toEqual(expected);
   });
 });
