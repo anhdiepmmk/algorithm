1e9905dd2b09ff3c2915a6c74ef9a218c4afb241
nammon72 <namnh.vst@gmail.com>
2022-07-17 11:24:23 +0700

    feat: draw the circle

--- src/draw-the-circle.js
+++ src/draw-the-circle.js
@@ -3,11 +3,11 @@ const drawCircle = (radius) => {
   let result = '';
   for (let y = radius; y >= -radius; y -= 1) {
     for (let x = radius; x >= -radius; x -= 1) {
-      const value = x ** 2 + y ** 2 - radius ** 2;
+      const value = x ** 2 + y ** 2 - rqadius ** 2;
       if (value < 0) {
         result += symbol;
       }
-      if (value == 0) {
+      if (value === 0) {
         result += '-';
       }
       if (value > 0) {
