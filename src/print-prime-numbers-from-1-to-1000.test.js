57d59718c51807d0b2cffe00d1860b6bdc456aa2
nammon72 <namnh.vst@gmail.com>
2022-07-19 21:38:01 +0700

    fix: refactor two file: .js and .test.js of 'print-prime-numbers-from-1-to-1000'

--- src/print-prime-numbers-from-1-to-1000.test.js
+++ src/print-prime-numbers-from-1-to-1000.test.js
@@ -1,8 +1,10 @@
 const primeNumber = require('./print-prime-numbers-from-1-to-1000');
+const checkPrimesService = require('./check-primes');
 
 describe('print prime numbers', () => {
   test('should return an array of prime numbers from 1 to 1000 when given n = 1000', () => {
     const n = 1000;
+    jest.spyOn(checkPrimesService, 'isPrimeNumber');
     const expectedResult = [
       2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67,
       71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149,
@@ -18,5 +20,6 @@ describe('print prime numbers', () => {
     ];
     const result = primeNumber.printPrime(n);
     expect(result).toEqual(expectedResult);
+    expect(checkPrimesService.isPrimeNumber).toHaveBeenCalledTimes(n);
   });
 });
