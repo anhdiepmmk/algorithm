3357c8727322e34b45113b9c7d8308bb01fd20a1
nammon72 <namnh.vst@gmail.com>
2022-07-24 23:12:52 +0700

    feat: create the function to check if a given number is perfect or not

---
+++ src/perfect-number.test.js
@@ -0,0 +1,20 @@
+const perfect = require('./perfect-number');
+
+describe('perfect number', () => {
+  test.each([
+    { n: 6, expected: true },
+    { n: 28, expected: true },
+    { n: 496, expected: true },
+    { n: 8128, expected: true },
+  ])('should return $expected when given $n', ({ n, expected }) => {
+    expect(perfect.isPerfect(n)).toEqual(expected);
+  });
+  test.each([
+    { n: 12, expected: false },
+    { n: 5, expected: false },
+    { n: 100, expected: false },
+    { n: 128, expected: false },
+  ])('should return $expected when given $n', ({ n, expected }) => {
+    expect(perfect.isPerfect(n)).toEqual(expected);
+  });
+});
